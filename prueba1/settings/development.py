fe5a840de9870934e2544a5212da7ff60a7fa6c5
danielt98 <detulande@unicauca.edu.co>
2021-11-15 15:55:49 -0500

    Migrations add

--- prueba1/settings/development.py
+++ prueba1/settings/development.py
@@ -9,7 +9,18 @@ ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+""""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT':5432
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
