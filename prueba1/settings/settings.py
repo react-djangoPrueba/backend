1bfb7efc1c48544e64dd4f17bb4199b569587c53
danielt98 <detulande@unicauca.edu.co>
2021-11-14 23:01:40 -0500

    heroku lb

--- prueba1/settings/settings.py
+++ prueba1/settings/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,6 +161,8 @@ GRAPHQL_JWT = {
     #"JWT_ALLOW_ARGUMENT": True,
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 """
 GRAPHQL_JWT = {
     "JWT_VERIFY_EXPIRATION": True,
